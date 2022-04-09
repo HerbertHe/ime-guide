69c6be9b6b4265103728a312f64c1e9da5ed34f5
Herbert <herbert.he0229@gmail.com>
2022-04-09 11:35:35 +0800

    chore :wrench: : fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vueI18n from "@intlify/vite-plugin-vue-i18n"
 import path from "path"
 
 export default defineConfig({
+    base: "/ime-guide/",
     plugins: [
         vueI18n({
             include: path.resolve(__dirname, "./locales/*"),
